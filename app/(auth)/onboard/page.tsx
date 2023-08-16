be8ee086767a04addbe6ebf8d46bb6962fac7544
Arjun Marar <arjunvmarar@gmail.com>
2023-08-16 23:44:38 +1000

    fetch user info from database

--- app/(auth)/onboard/page.tsx
+++ app/(auth)/onboard/page.tsx
@@ -1,10 +1,15 @@
-import AccountProfile from '@/components/forms/AccountProfile';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import AccountProfile from '@/components/forms/AccountProfile';
+import { fetchUser } from '@/lib/actions/user.actions';
 
 const Page = async () => {
     const user = await currentUser();
+    if (!user) return null;
 
-    const userInfo = {};
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect('/');
 
     const userData = {
         id: user?.id,
@@ -17,7 +22,7 @@ const Page = async () => {
     return (
         <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
-            <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Sutra</p>
+            <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use Sutram</p>
             <section className="mt-9 bg-dark-2 p-10">
                 <AccountProfile user={userData} btnTitle="Continue" />
             </section>
