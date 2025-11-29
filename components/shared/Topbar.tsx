44b0ec0030d359d97d96390582b87712d25e2dc3
Arjun Marar <arjunvmarar@gmail.com>
2025-11-29 20:03:01 +1100

    style: polish topbar and clerk dropdown

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -24,7 +24,24 @@ const Topbar = () => {
                     appearance={{
                         baseTheme: dark,
                         elements: {
-                            organizationSwitcherTrigger: 'py-2 px-4'
+                            organizationSwitcherTrigger:
+                                'py-2 px-4 bg-dark-3 text-light-1 border border-dark-4 hover:bg-dark-2 transition data-[state=open]:bg-dark-2 data-[state=open]:text-light-1 data-[active=true]:text-light-1 !text-light-1',
+                            organizationSwitcherTriggerIcon:
+                                'text-light-1 group-hover:text-light-1 data-[state=open]:text-light-1 data-[active=true]:text-light-1 !text-light-1',
+                            organizationSwitcherTriggerText:
+                                'text-light-1 hover:text-light-1 data-[state=open]:text-light-1 data-[active=true]:text-light-1 !text-light-1',
+                            organizationSwitcherPopoverCard: 'bg-dark-3 border border-dark-4 text-light-1',
+                            organizationSwitcherPopoverHeader: 'text-light-1',
+                            organizationSwitcherPopoverText: 'text-light-1',
+                            organizationSwitcherPopoverSubtitle: 'text-light-2',
+                            organizationSwitcherPopoverItem: 'text-light-1 hover:bg-dark-2',
+                            organizationSwitcherPopoverActionButton:
+                                'text-light-1 hover:bg-dark-2 border border-dark-4',
+                            organizationSwitcherPopoverCreateOrganization:
+                                'text-light-1 hover:bg-dark-2 border border-dark-4',
+                            organizationSwitcherPopoverManageOrganization:
+                                'text-light-1 hover:bg-dark-2 border border-dark-4',
+                            organizationSwitcherPopoverPrimaryButton: 'bg-primary-500 hover:bg-primary-600 text-white'
                         }
                     }}
                 />
